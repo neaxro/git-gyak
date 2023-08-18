b577c8163616da3459376e116436b3a22303523c
Axel Roland Nemes <neaxro@gmail.com>
2023-08-18 15:30:08 +0200

    Create sum file

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+import sum
+
 def main():
 	for i in range(1, 10):
 		print(i)
+		print(sum.add(i, 10))
 
 
 if __name__ == '__main__':
---
+++ sum.py
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+def add(a, b):
+	return a + b
+
+def sub(a, b):
+	return a - b
+
+def mul(a, b):
+	return a * b
+
+def div(a, b):
+	if b != 0:
+		return a / b
+	return "Error"
