c5a864f0fe7586564d1b015d3558385138876c4a
Axel Roland Nemes <neaxro@gmail.com>
2023-08-18 15:33:19 +0200

    Only parosak

--- hello.py
+++ hello.py
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-import sum
+import sum as sum_modul
 
 def main():
 	for i in range(1, 10):
 		print(i)
-		print(sum.add(i, 10))
+		sum = sum_modul.add(i, 10)
+
+		if sum % 2 == 0:
+			print("Sum:", sum)
+		else:
+			print("Sum:", (sum + 1))
 
 
 if __name__ == '__main__':
