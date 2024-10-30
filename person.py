8e5e32ffec2f797cad9848ec59c0667e763e2c1d
Axel Roland Nemes <neaxro@gmail.com>
2024-10-30 18:06:33 +0100

    Modified Hi to Hello

--- person.py
+++ person.py
@@ -4,9 +4,9 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-    
+
     def hello(self):
-        print("Hi my name is", self.name, "and I am ", self.age, "year(s) old!")
+        print("Hello my name is", self.name, "and I am ", self.age, "year(s) old!")
 
     def addNumbers(a, b):
         s = sum.add(a, b)
